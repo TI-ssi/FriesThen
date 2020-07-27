6a4597e1361bb64359c77a981597884d81dbb067
Skarn <skarn@ti-ssi.com>
2020-07-27 18:06:40 +0000

    fix setup

--- sample.config.php
+++ sample.config.php
@@ -7,5 +7,6 @@
   */
 
 return [
-       'ver' => '';
+       'ver' => '',
+       'debug' => false       
 ]
--- setup.php
+++ setup.php
@@ -1,5 +1,11 @@
 <?php
 
-$config = include 'config.php';
+$config = include('config.php');
+
+if($config['debug']) {
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+}
 
 $ver = empty($config['ver'])? $config['ver'] : time();
